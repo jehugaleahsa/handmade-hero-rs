b10ce5c0deef3c071d89b5ab0948d28d2ab647b7
Travis Parks <travis@aligrade.com>
2026-08-31 19:41:36 -0400

    Commit missing IntializeContext changes

--- handmade_hero_interface/src/initialize_context.rs
+++ handmade_hero_interface/src/initialize_context.rs
@@ -1,6 +1,7 @@
-use crate::game_state::GameState;
+use crate::{back_buffer::BackBuffer, game_state::GameState};
 
 #[derive(Debug)]
 pub struct InitializeContext<'a> {
     pub state: &'a mut GameState,
+    pub back_buffer: &'a mut BackBuffer,
 }
